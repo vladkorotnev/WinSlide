e33fc64b990db0785f0e71802f96cce1de753e05
Vladislav Korotnev <vladkorotnev@gmail.com>
2011-08-05 20:33:29 +0600

    init

---
+++ App/Winlock/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.topbar = New System.Windows.Forms.Panel
+        Me.daten = New System.Windows.Forms.Label
+        Me.clocks = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.lockima = New System.Windows.Forms.ImageList(Me.components)
+        Me.animator = New System.Windows.Forms.Timer(Me.components)
+        Me.bottombar = New System.Windows.Forms.Panel
+        Me.bottomlocker = New System.Windows.Forms.Panel
+        Me.ololo = New System.Windows.Forms.PictureBox
+        Me.lock = New System.Windows.Forms.PictureBox
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox
+        Me.blackie = New System.Windows.Forms.Panel
+        Me.blankscreen = New System.Windows.Forms.Timer(Me.components)
+        Me.animout = New System.Windows.Forms.Timer(Me.components)
+        Me.topbar.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.bottombar.SuspendLayout()
+        Me.bottomlocker.SuspendLayout()
+        CType(Me.ololo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.lock, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'topbar
+        '
+        Me.topbar.Controls.Add(Me.daten)
+        Me.topbar.Controls.Add(Me.clocks)
+        Me.topbar.Controls.Add(Me.PictureBox1)
+        Me.topbar.Location = New System.Drawing.Point(0, 2)
+        Me.topbar.Name = "topbar"
+        Me.topbar.Size = New System.Drawing.Size(540, 76)
+        Me.topbar.TabIndex = 1
+        '
+        'daten
+        '
+        Me.daten.AutoSize = True
+        Me.daten.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.daten.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.daten.ForeColor = System.Drawing.Color.White
+        Me.daten.Location = New System.Drawing.Point(210, 41)
+        Me.daten.Name = "daten"
+        Me.daten.Size = New System.Drawing.Size(89, 20)
+        Me.daten.TabIndex = 3
+        Me.daten.Text = "10.01.2011"
+        '
+        'clocks
+        '
+        Me.clocks.AutoSize = True
+        Me.clocks.BackColor = System.Drawing.Color.DarkGray
+        Me.clocks.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.clocks.ForeColor = System.Drawing.Color.White
+        Me.clocks.Location = New System.Drawing.Point(222, 7)
+        Me.clocks.Name = "clocks"
+        Me.clocks.Size = New System.Drawing.Size(64, 24)
+        Me.clocks.TabIndex = 2
+        Me.clocks.Text = "00:00"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.topbar
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(545, 77)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'lockima
+        '
+        Me.lockima.ImageStream = CType(resources.GetObject("lockima.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.lockima.TransparentColor = System.Drawing.Color.Transparent
+        Me.lockima.Images.SetKeyName(0, "1")
+        Me.lockima.Images.SetKeyName(1, "2")
+        Me.lockima.Images.SetKeyName(2, "3")
+        Me.lockima.Images.SetKeyName(3, "4")
+        Me.lockima.Images.SetKeyName(4, "5")
+        Me.lockima.Images.SetKeyName(5, "6.bmp")
+        Me.lockima.Images.SetKeyName(6, "7.bmp")
+        Me.lockima.Images.SetKeyName(7, "8.bmp")
+        Me.lockima.Images.SetKeyName(8, "9.bmp")
+        Me.lockima.Images.SetKeyName(9, "20.bmp")
+        Me.lockima.Images.SetKeyName(10, "10.bmp")
+        Me.lockima.Images.SetKeyName(11, "11.bmp")
+        Me.lockima.Images.SetKeyName(12, "12.bmp")
+        Me.lockima.Images.SetKeyName(13, "13.bmp")
+        Me.lockima.Images.SetKeyName(14, "14.bmp")
+        Me.lockima.Images.SetKeyName(15, "15.bmp")
+        Me.lockima.Images.SetKeyName(16, "16.bmp")
+        Me.lockima.Images.SetKeyName(17, "17.bmp")
+        Me.lockima.Images.SetKeyName(18, "18.bmp")
+        Me.lockima.Images.SetKeyName(19, "19.bmp")
+        '
+        'animator
+        '
+        Me.animator.Enabled = True
+        '
+        'bottombar
+        '
+        Me.bottombar.Controls.Add(Me.bottomlocker)
+        Me.bottombar.Controls.Add(Me.PictureBox2)
+        Me.bottombar.Location = New System.Drawing.Point(0, 392)
+        Me.bottombar.Name = "bottombar"
+        Me.bottombar.Size = New System.Drawing.Size(543, 78)
+        Me.bottombar.TabIndex = 3
+        '
+        'bottomlocker
+        '
+        Me.bottomlocker.Controls.Add(Me.ololo)
+        Me.bottomlocker.Controls.Add(Me.lock)
+        Me.bottomlocker.Location = New System.Drawing.Point(169, 6)
+        Me.bottomlocker.Name = "bottomlocker"
+        Me.bottomlocker.Size = New System.Drawing.Size(205, 64)
+        Me.bottomlocker.TabIndex = 6
+        '
+        'ololo
+        '
+        Me.ololo.Image = Global.WindowsApplication1.My.Resources.Resources.arrow_black
+        Me.ololo.Location = New System.Drawing.Point(8, 18)
+        Me.ololo.Name = "ololo"
+        Me.ololo.Size = New System.Drawing.Size(48, 32)
+        Me.ololo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.ololo.TabIndex = 7
+        Me.ololo.TabStop = False
+        '
+        'lock
+        '
+        Me.lock.Image = Global.WindowsApplication1.My.Resources.Resources._1
+        Me.lock.Location = New System.Drawing.Point(-9, 0)
+        Me.lock.Name = "lock"
+        Me.lock.Size = New System.Drawing.Size(224, 67)
+        Me.lock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.lock.TabIndex = 6
+        Me.lock.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.topbar
+        Me.PictureBox2.Location = New System.Drawing.Point(-2, 1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(545, 77)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'blackie
+        '
+        Me.blackie.BackColor = System.Drawing.Color.Black
+        Me.blackie.Location = New System.Drawing.Point(145, 302)
+        Me.blackie.Name = "blackie"
+        Me.blackie.Size = New System.Drawing.Size(32, 28)
+        Me.blackie.TabIndex = 4
+        Me.blackie.Visible = False
+        '
+        'blankscreen
+        '
+        Me.blankscreen.Enabled = True
+        Me.blankscreen.Interval = 8000
+        '
+        'animout
+        '
+        Me.animout.Enabled = True
+        Me.animout.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(542, 470)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.blackie)
+        Me.Controls.Add(Me.bottombar)
+        Me.Controls.Add(Me.topbar)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.TransparencyKey = System.Drawing.Color.Red
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.topbar.ResumeLayout(False)
+        Me.topbar.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.bottombar.ResumeLayout(False)
+        Me.bottomlocker.ResumeLayout(False)
+        Me.bottomlocker.PerformLayout()
+        CType(Me.ololo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.lock, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents topbar As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents clocks As System.Windows.Forms.Label
+    Friend WithEvents lockima As System.Windows.Forms.ImageList
+    Friend WithEvents animator As System.Windows.Forms.Timer
+    Friend WithEvents bottombar As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents bottomlocker As System.Windows.Forms.Panel
+    Friend WithEvents ololo As System.Windows.Forms.PictureBox
+    Friend WithEvents lock As System.Windows.Forms.PictureBox
+    Friend WithEvents daten As System.Windows.Forms.Label
+    Friend WithEvents blackie As System.Windows.Forms.Panel
+    Friend WithEvents blankscreen As System.Windows.Forms.Timer
+    Friend WithEvents animout As System.Windows.Forms.Timer
+
+End Class
---
+++ App/Winlock/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Dim animcnt As Integer
+    Dim animax As Integer
+    Dim keyspressed As Integer
+    Dim shouldanimateslideout As Boolean
+    '-----------------------------------
+    '       Screen blank
+    '-----------------------------------
+
+    Sub blankScr()
+        blackie.Visible = True
+        blankscreen.Enabled = False
+        animator.Enabled = False
+        Windows.Forms.Cursor.Hide()
+    End Sub
+
+    Sub unblankScr()
+        If blackie.Visible Then
+            blackie.Visible = False
+            blankscreen.Enabled = True
+            animator.Enabled = True
+            Windows.Forms.Cursor.Show()
+        End If
+    End Sub
+
+
+
+
+    Private Sub animator_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles animator.Tick
+        If animcnt < animax Then
+            lock.Image = lockima.Images.Item(animcnt)
+            animcnt = animcnt + 1
+        Else
+            animcnt = 1
+        End If
+        clocks.Text = TimeString
+        daten.Text = DateString
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+
+        If blackie.Visible Then
+            unblankScr()
+            Exit Sub
+        End If
+
+        keyspressed = keyspressed + 1
+        ololo.Left = ololo.Left + 15
+        If ololo.Left >= lock.Left + lock.Width - ololo.Width - 20 Then
+            shouldanimateslideout = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        animax = lockima.Images.Count
+        animcnt = 1
+
+        Dim sndPing As New System.Media.SoundPlayer("lock.wav")
+        sndPing.Play()
+
+        On Error Resume Next
+        Me.BackgroundImage = System.Drawing.Image.FromFile("C:\LockWall.jpg")
+
+        Me.TopMost = True
+
+        '------------------------------------------------
+        '               TOP SIZE
+        '------------------------------------------------
+        PictureBox1.Width = Screen.PrimaryScreen.Bounds.Width
+        topbar.Width = Screen.PrimaryScreen.Bounds.Width
+        clocks.Left = topbar.Width / 2 - clocks.Width
+        daten.Left = clocks.Left
+
+        '-----------------------------------------------
+        '               BOTTOM SIZE
+        '-----------------------------------------------
+        bottombar.Width = Screen.PrimaryScreen.Bounds.Width
+        bottombar.Top = Me.Height - bottombar.Height
+        PictureBox2.Width = Screen.PrimaryScreen.Bounds.Width
+        bottomlocker.Left = PictureBox2.Width / 2 - bottomlocker.Width / 2
+
+        '-----------------------------------------------
+        '               SCREEN BLANKER
+        '-----------------------------------------------
+        blackie.Width = Screen.PrimaryScreen.Bounds.Width
+        blackie.Height = Screen.PrimaryScreen.Bounds.Height
+        blackie.Left = 0
+        blackie.Top = 0
+    End Sub
+
+    Private Sub blankscreen_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles blankscreen.Tick
+        blankScr()
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        unblankScr()
+    End Sub
+
+    Private Sub blackie_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles blackie.MouseDown
+        unblankScr()
+    End Sub
+
+
+    Private Sub animout_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles animout.Tick
+
+        If shouldanimateslideout And topbar.Top > 0 - topbar.Height Then
+            topbar.Top = topbar.Top - 7
+            bottombar.Top = bottombar.Top + 7
+            Me.Opacity = Me.Opacity - 0.08
+            unblankScr()
+        ElseIf shouldanimateslideout And topbar.Top <= 0 - topbar.Height Then
+            Dim sndPing As New System.Media.SoundPlayer("unlock.wav")
+            sndPing.PlaySync()
+            End
+        End If
+    End Sub
+End Class
